f886e4d4b009505dc86572153481ef5ab143fa31
Dzemil Alendar <dzem.alen@gmail.com>
2023-11-03 13:08:01 +0100

    oppdaterte serive model og tilhørende views

---
+++ Nsted/Migrations/20231103111752_NewChanges.cs
@@ -0,0 +1,84 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Nsted.Migrations
+{
+    /// <inheritdoc />
+    public partial class NewChanges : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "ServiceNotat",
+                table: "Servicer");
+
+            migrationBuilder.AddColumn<string>(
+                name: "Notat",
+                table: "Servicer",
+                type: "longtext",
+                nullable: false)
+                .Annotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.AddColumn<string>(
+                name: "Produkttype",
+                table: "Servicer",
+                type: "longtext",
+                nullable: false)
+                .Annotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.AddColumn<string>(
+                name: "Serienummer",
+                table: "Servicer",
+                type: "longtext",
+                nullable: false)
+                .Annotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.AddColumn<string>(
+                name: "ServiceRep",
+                table: "Servicer",
+                type: "longtext",
+                nullable: false)
+                .Annotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.AddColumn<int>(
+                name: "Årsmodell",
+                table: "Servicer",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Notat",
+                table: "Servicer");
+
+            migrationBuilder.DropColumn(
+                name: "Produkttype",
+                table: "Servicer");
+
+            migrationBuilder.DropColumn(
+                name: "Serienummer",
+                table: "Servicer");
+
+            migrationBuilder.DropColumn(
+                name: "ServiceRep",
+                table: "Servicer");
+
+            migrationBuilder.DropColumn(
+                name: "Årsmodell",
+                table: "Servicer");
+
+            migrationBuilder.AddColumn<string>(
+                name: "ServiceNotat",
+                table: "Servicer",
+                type: "longtext",
+                nullable: true)
+                .Annotation("MySql:CharSet", "utf8mb4");
+        }
+    }
+}

59bbeed64fc16f1b1da93dc2aeffaa7e318b0572
NoraGith <noraolo@uia.no>
2023-11-25 17:53:59 +0100

    kommentering av service

--- Nsted/Controllers/ServiceController.cs
+++ Nsted/Controllers/ServiceController.cs
@@ -7,11 +7,12 @@ using System.Text.Encodings.Web;
 
 namespace Nsted.Controllers
 {
+    //Kontroller som kummuniserer med ServiceRepository 
     [Authorize]
     public class ServiceController : Controller
     {
 
-        //Constructor for repository
+        //Konstruktør for repository som bruker IserviceRepository som en instans
         private readonly IServiceRepository serviceRepository;
 
         public ServiceController(IServiceRepository serviceRepository)
@@ -20,13 +21,13 @@ namespace Nsted.Controllers
             this.serviceRepository = serviceRepository;
         }
 
-
+        //Add-metode for å returnere et view 
         public IActionResult Add()
         {
             return View();
         }
 
-        
+        //Add metode som tar imot instanser fra Service og viser view
         [HttpPost]
         [ValidateAntiForgeryToken]
         public async Task<IActionResult> Add(Service service)
@@ -36,6 +37,9 @@ namespace Nsted.Controllers
             //redirect to the list page
             return RedirectToAction("List");
         }
+
+        //Henter alle ut all informasjon fra Servicer-tabellen, setter det i en variabel og returnerer dette til view 
+        [HttpGet]
         public async Task<IActionResult> List()
         {
             var servicer = await serviceRepository.GetAllAsync();
@@ -43,6 +47,7 @@ namespace Nsted.Controllers
             return View(servicer);
         }
 
+        //Tar imot en instans av Service, hetner informasjon fra ServiceRepository for å slette barest på ID. Omdirigerer til view List
         [HttpPost]
         [ValidateAntiForgeryToken]
         public async Task<IActionResult> Delete(Service service)
@@ -59,7 +64,7 @@ namespace Nsted.Controllers
             return View(null);
         }
 
-        //Hvordan fungerer id parameteren her?
+        //Håndterer visingen for redigering basert på gitt id. Dersom den ikke er null blir edit viewet vist
         [HttpGet]
         public async Task<IActionResult> Edit(int id)
         {
@@ -75,6 +80,7 @@ namespace Nsted.Controllers
             return View(null);
         }
 
+        //Håndeter visningen for gitt søkeresultat, viser bare relevante søkeresultat 
         [HttpGet]
         public async Task<IActionResult> Search(string searchString)
         {
@@ -88,6 +94,7 @@ namespace Nsted.Controllers
             return View("List", service);
         }
 
+        //Håndterer visningen for søkeresultatet basert på status. Filtrere etter status
         [HttpGet]
         public async Task<IActionResult> SearchStatus(string searchString)
         {
@@ -101,6 +108,7 @@ namespace Nsted.Controllers
             return View("List", service);
         }
 
+        //Håndterer lagringen av endringer i tabellen etter redigering. Dersom den ikke er lik null blir viewet redirigert til List
         [HttpPost]
         [ValidateAntiForgeryToken]
         public async Task<IActionResult> Edit(Service service)

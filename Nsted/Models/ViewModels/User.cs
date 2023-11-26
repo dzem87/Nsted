47ba763365ea3bb0c7bf1295ec3ccca728e6afba
Anita Øygard <anitaoygard@Anitas-MacBook-Pro.local>
2023-11-26 13:50:37 +0100

    La til kommentarer i en del filer

--- Nsted/Models/ViewModels/User.cs
+++ Nsted/Models/ViewModels/User.cs
@@ -5,8 +5,13 @@ namespace Nsted.Models.ViewModels
 
 	public class User
 	{
+        // Unik ID for brukeren.
         public Guid Id { get; set; }
+        
+        // Brukernavn for brukeren.
         public string Username { get; set; }
+        
+        // E-postadresse knyttet til brukeren.
         public string Email { get; set; }
     }
 }
--- Nsted/Models/ViewModels/UserViewModel.cs
+++ Nsted/Models/ViewModels/UserViewModel.cs
@@ -6,11 +6,20 @@ namespace Nsted.Models.ViewModels
 {
 	public class UserViewModel
 	{
+		
+		// Liste av brukere som overføres til eller fra viewet.
 		public List<User> Users { get; set; }
-
+		
+		// Brukernavn til en enkelt bruker.
 		public string Username { get; set; }
+		
+		// E-postadresse til en enkelt bruker.
         public string Email { get; set; }
+        
+        // Passord til en enkelt bruker.
         public string Password { get; set; }
+        
+        // Checkbox som indikerer om brukeren har administratortilgang eller ikke.
         public bool AdminRoleCheckbox { get; set; }
     }
 }
